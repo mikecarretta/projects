e6ce243ac97a94e6e531eb872462d7d24a629dac
Mike Carretta <mikecarretta@gmail.com>
2014-03-30 21:08:40 -0400

    weather app updates

---
+++ weather/app-local.js
@@ -0,0 +1,77 @@
+// wunderground.com  api  dc78f7e3ed16d1b7
+// http://api.wunderground.com/api/dc78f7e3ed16d1b7/forecast/q/06615.json
+// forecast.simpleforecast.forecastday / data , high.fahrenheit , conditions
+(function() {
+  var App = {
+    init: function(config) {
+      this.zip();
+    },
+
+    zip: function() {
+      var that = this;
+      $('#submit').on('click', function(){
+        $('.message').addClass('hide');
+        var zipVal = $('#zipcode').val();
+
+        if (zipVal.length === 5) {
+          return that.api();
+          // console.log(that);
+        } else {
+          // Error Message
+          $('.message').removeClass('hide').html('').append(
+            zipVal + ' is not a zip code. Please enter 6 numbers.')
+        }
+      })
+    },
+
+    api: function() {
+      var self = this;
+      var apiUrl = '06615.json';
+
+      $.getJSON( apiUrl, function(data) {
+        var fiveDayUrl = data.forecast.simpleforecast.forecastday;
+
+        self.records = $.map(fiveDayUrl, function(apiData) {
+          return {
+            day: apiData.date.day,
+            mo: apiData.date.monthname_short,
+            yr: apiData.date.year,
+            hi: apiData.high.fahrenheit,
+            low: apiData.low.fahrenheit,
+            icon: apiData.icon
+          };
+        });
+        self.attachTemplate();
+      });
+    },
+
+    attachTemplate: function() {
+      console.log(this.records);
+
+      var source   = $("#weather").html();
+      var template = Handlebars.compile(source);
+      $(".forecast").html(template( this.records ));
+    }
+  };
+
+  App.init();
+
+})();
+
+/*
+zipVal.('^[0-9]+$')
+
+  onclick
+  if value.length = 5 & numeric
+  else display error message
+  get input value from text field
+  display spinner
+  make ajax call
+  on success display temp with icons
+  change background color
+    case
+      0-49 blue
+      50-70 orange
+      71-89 orange-red
+      90-100+ red
+*/
